3c004d9eb7e632f611cd2a32c9b53f915f2425da
Ahmed <ahmedzeinstu@gmail.com>
2024-05-28 16:20:59 +0100

    E-commerce-API

--- config/database.js
+++ config/database.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 
 const dbConnection = () => {
   mongoose
-    .connect(process.env.DB_URL)
+    .connect(
+      "mongodb+srv://ahmedzeintsu:WbcdS0CaYK7jPbZq@cluster0.1uj7ugs.mongodb.net/E_commerce?retryWrites=true&w=majority"
+    )
     .then((con) => {
-      console.log(`Mogoose Host :${  con.connection.host}`);
-    })
+      console.log(`Mogoose Host :${con.connection.host}`);
+    });
 
 };
 module.exports = dbConnection;
