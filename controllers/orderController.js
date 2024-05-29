7ede1a964ffda3af674636246d1a13dae28345fa
Ahmed <ahmedzeinstu@gmail.com>
2024-05-29 17:39:03 +0100

    add webhook

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -197,7 +197,7 @@ exports.webhookCheckout = asyncHandler(async (req, res) => {
   // Handle the event
   if (event.type === "checkout.session.completed") {
     console.log("create order here......");
-    console.log(event.data.object.client.reference_id);
+    console.log(event.data.object);
   }
   // Return a 200 response to acknowledge receipt of the event
   res.send();
